a2a088a6ac2be21979308110a9e685e0e4d69c44
jjaneyxx <hmyeonge@gmail.com>
2024-10-25 17:42:34 +0900

    refactor: getCityName() 를 async/await 으로 리팩토링

--- src/main.js
+++ src/main.js
@@ -21,23 +21,22 @@ function error() {
 }
 
 // 현재 위치의 도시 이름을 화면에 출력하는 함수
-function getCityName(latitude, longitude) {
+async function getCityName(latitude, longitude) {
   const apiKey = config.apiKey;
-  console.log(apiKey);
+
   // fetch 요청을 보낼 주소
   const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${apiKey}`;
   console.log("url", url);
 
-  // url 을 기반으로 API 에 데이터를 받아오도록 요청
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      // data 가 true 면
-      console.log("data", data);
-      if (data.status === "OK") {
-        const address = data.results[7].formatted_address; // 대한민국 경기도 고양시
-        document.getElementById("city").innerText = `${address}`; // 위치를 화면에 표시
-      }
-    })
-    .catch((error) => console.log("에러: ", error));
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    if (data.status === "OK") {
+      const address = data.results[7].formatted_address; // 대한민국 경기도 고양시
+      document.getElementById("city").innerText = `${address}`; // 위치를 화면에 표시
+    }
+  } catch (error) {
+    console.log("에러: ", error);
+  }
 }
